8eb07f6921e65efb59c34f5ee786a5598d7cb916
zhakhov <zhakhov@ZHAKOV>
2016-07-28 17:22:20 +0400

    asd

--- App7/App7.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ App7/App7.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,6 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("App7.Droid.MainApplication, App7.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md533cea878de1e2c50548e44cd1a7b927b.MainApplication.class, md533cea878de1e2c50548e44cd1a7b927b.MainApplication.__md_methods);
 		
 	}
 }
--- App7/App7/TodoListPageCS.cs
+++ App7/App7/TodoListPageCS.cs
@@ -1,23 +1,104 @@
 ﻿using Xamarin.Forms;
+using Plugin.Geolocator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace App7
 {
 	public class TodoListPageCS : ContentPage
 	{
-		public TodoListPageCS ()
-		{
-			Title = "TodoList Page";
+        double distance = 0;
+        double lat1 = 0;
+        double lon1 = 0;
+        Label l1 = new Label
+        {
+            Text = "Todo list data goes here",
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.CenterAndExpand
+        };
+
+        public TodoListPageCS ()
+        {
+            var buttonGetGPS = new Button
+            {
+                Text = "GetGPS"
+            };
+
+            var labelGPS = new Label
+            {
+                Text = "GPS goes here"
+            };
+            var locator = CrossGeolocator.Current;
+            buttonGetGPS.Clicked += async (sender, args) =>
+            {
+              
+                locator.DesiredAccuracy = 50;
+                labelGPS.Text = "Getting gps";
+
+                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+                if (position == null)
+                {
+                    labelGPS.Text = "null gps :(";
+                    return;
+                }
+                labelGPS.Text = string.Format("Time: {0} \nLat: {1} \nLong: {2} \n Altitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \n Heading: {6} \n Speed: {7}",
+                  position.Timestamp, position.Latitude, position.Longitude,
+                  position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
+                await locator.StartListeningAsync(minTime: 1000, minDistance: 0);
+                locator.PositionChanged += (sender2, e) => {
+                   
+
+                    position = e.Position;
+                    if (lat1 == 0)
+                    {
+                        lat1 = position.Latitude;
+                        lon1 = position.Longitude;
+                    }
+                    else {
+                        distance += GPS.Utils.distance(lat1, lon1, position.Latitude, position.Longitude, 'K');
+                        lat1 = position.Latitude;
+                        lon1 = position.Longitude;
+                    }
+                    labelGPS.Text = string.Format("Time: {0} \nLat: {1} \nLong: {2} \n Altitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \n Heading: {6} \n Speed: {7}  \n Distance: {8} ",
+                  position.Timestamp, position.Latitude, position.Longitude,
+                  position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed,distance);
+                   
+                };
+            };
+
+
+            Title = "TodoList Page";
             BackgroundColor = Color.White;
            
 			Content = new StackLayout { 
 				Children = {
-					new Label {
-						Text = "Todo list data goes here",
-						HorizontalOptions = LayoutOptions.Center,
-						VerticalOptions = LayoutOptions.CenterAndExpand
-					}
-				}
+                buttonGetGPS,
+            labelGPS
+                }
 			};
-		}
-	}
+          
+
+        }
+        public async void RunGPS() {/*
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+
+                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+                Console.WriteLine("Position Status: {0}", position.Timestamp);
+                Console.WriteLine("Position Latitude: {0}", position.Latitude);
+                Console.WriteLine("Position Longitude: {0}", position.Longitude);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Unable to get location, may need to increase timeout: " + ex);
+            }
+       */ }
+    }
 }
